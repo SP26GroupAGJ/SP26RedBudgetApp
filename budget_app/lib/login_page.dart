496492793b50a26799a8937cf99524535428496d
JoshW90602 <125218854+JoshW90602@users.noreply.github.com>
2024-10-08 15:31:43 -0400

    Add files via upload
    added username and password functionality

--- budget_app/lib/login_page.dart
+++ budget_app/lib/login_page.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  final String username = "testUser";
+  final String password = "testPass";
+  final userController = TextEditingController();
+  final passController = TextEditingController();
+  
+  @override
+  void dispose(){
+    userController.dispose();
+    passController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +37,25 @@ class LoginPage extends StatelessWidget {
             ),
             ElevatedButton(
                 onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/login');
+                  if(userController.text==username && passController.text==password){
+                    Navigator.pushReplacementNamed(context, '/login');
+                  }
+                  else{
+                    
+                  }
+                  
                 },
                 child: Text("To Home Screen")),
+            Padding(padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+            child: TextFormField(
+              decoration: const InputDecoration(border: UnderlineInputBorder(), labelText: 'Username'),
+              controller : userController,
+              ),),
+            Padding(padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+            child: TextFormField(
+              decoration: const InputDecoration(border: UnderlineInputBorder(), labelText: 'Password'),
+              controller: passController,
+            ),),
             ElevatedButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/create_accnt');
